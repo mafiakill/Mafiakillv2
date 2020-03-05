eedff1e39dc5414be590a4959b75fc31b070ab4c
mafiakill <61772007+mafiakill@users.noreply.github.com>
2020-03-05 06:07:55 +0100

    Add files via upload
    Add bank.php to the lang

---
+++ en.php
@@ -0,0 +1,62 @@
+<?php
+
+
+
+
+//_gevagenis.inc
+$gevangtxt = "You are in jail! </b> <br>They let you go in";
+$gevangtxt1 = "secondes";
+$gevanguser = "Username:";
+$gevangclan = "Clan:";
+$gevangguarantee = "Guarantee:";
+$gevangtijd = "Time:";
+$gevangbprice = "Breakout price:";
+$gevangtextx = "Not possible because you are in jail!";
+$gevangtextxx = "Not possible you cannot break out yourself!";
+$gevangbuyout = "Buyout";
+$gevangbreakout = "Breakout";
+$gevanglegend = "Legend";
+
+//jail.php
+$jailtitle = "Jail";
+$jailtxt = "Our jail customers are here. You can buy or break this out. You can only break out if you are <b> not yet </b> in jail. If it fails you end up in jail!";
+$jailtxt1 = "You need to buy someone out.";
+$jailtxt2 = "You need ";
+$jailtxt3 = "on your cash to buyout!";
+$jailtxt4 = "You have buyout";
+$jailtxt5 = "you have pay";
+$jailtxt6 = "His price for breakout is to hight, He dont have the money on his bank.";
+$jailtxt7 = "When you blew up the wall you saw that nobody was in jail!";
+$jailtxt8 = "When you tried to break out yourself you were caught by 3 guards! You've gotten a straitjacket and your time has doubled!";
+$jailtxt9 = "You have breakout";
+$jailtxt10 = "It failed! You are now also in jail!";
+$jailtxt11 = "Breakout price";
+$jailtxt12 = "You don't have that much money in the bank.";
+$jailtxt13 = "Breakout price changed";
+$jailtxt14 = "Money you give to person that break you out of the jail.";
+$jailtxt15 = "Change";
+
+//bank.php
+$banktitle = "Bank";
+$bankdepwit = "Deposit or Withdraw";
+$banksent = "Send money";
+$bankop1 = "You have take";
+$bank2 = "from your bank.";
+$bank3 = "You don't have enough money in the bank";
+$bank4 = "You have transferred";
+$bank5 = "to your bank.";
+$bank6 = "You don't have that much money in your cash.";
+$bankcash = "Cash";
+$bankbank = "Bank";
+$banktakeall = "Take All";
+$bank7 = "Withdraw";
+$bank8 = "Deposit";
+$bank9 = "The username you entered is incorrect!";
+$bank10 = "You have send";
+$bank11 = "To";
+$bank12 = "You don't have enough money";
+$bank13 = "Amount";
+$bank14 = "send";
+$bank15 = "Donate clan";
+
+?>
